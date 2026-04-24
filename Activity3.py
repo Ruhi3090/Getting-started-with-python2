5714f5d8896b0e05e02b2796e7c6b6e8d74b26f6
Ruhi3090 <rm1204.kig@ked.edu.in>
2026-04-24 18:44:23 +0530

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,12 @@
+print("Hi,Welcome to the calculator.com")
+num1=int(input("Enter your first number:"))
+num2=int(input("Enter your second number:"))
+add=num1+num2
+print(add)
+sub=num1-num2
+print(sub)
+mult=num1*num2
+print(mult)
+divid=num1/num2
+print(divid)
+print("Thank You for using calculator.com You may go now")
